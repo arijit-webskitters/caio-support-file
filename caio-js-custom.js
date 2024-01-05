a92ebe73618cc6e086328f1ebe12c81e67aa028b
arijit-webskitters <155645185+arijit-webskitters@users.noreply.github.com>
2024-01-05 18:04:47 +0530

    old

--- caio-js-custom.js
+++ caio-js-custom.js
@@ -5,9 +5,7 @@ Webflow.push(function () {
     window.history.scrollRestoration = "manual";
     ScrollTrigger.clearScrollMemory();
     window.scrollTo(0, 0);
-
     function page_anim() {
-        
         //title/paragraph text anim
         if ($("[data-effect]").length) {
             let el_effects = document.querySelectorAll("[data-effect]");
@@ -19,10 +17,155 @@ Webflow.push(function () {
                 });
                 //   console.log(t.dataset.effect);
                 let pos = t.getBoundingClientRect();
-
                 function animload() {
+                    Splitting({ target: t, by: "chars" });
+                    gsap.set(t, { opacity: 0 });
+                    if (t.dataset.effect == "title") {
+                        const e = t.querySelectorAll(".word");
+                        e.forEach((m) =>
+                            gsap.set(m.parentNode, {
+                                perspective: 1e3,
+                            })
+                        );
+                        Tl = gsap.timeline({
+                            defaults: {
+                                ease: "Power3.easeOut",
+                                duration: 0.7,
+                            },
+                        });
+                        Tl.set(t, { opacity: 1 });
+                        Tl.fromTo(
+                            e,
+                            {
+                                "will-change": "opacity, transform",
+                                opacity: 0,
+                                rotateX: () => gsap.utils.random(-120, 120),
+                                z: () => gsap.utils.random(-200, 200),
+                                filter: "blur(10px)",
+                                scale: () => gsap.utils.random(-0.5, 1.5),
+                            },
+                            {
+                                opacity: 1,
+                                rotateX: 0,
+                                scale: 1,
+                                z: 0,
+                                filter: "none",
+                                stagger: 0.06,
+                            }
+                        ).pause();
+                        if (pos.top < window.innerHeight) {
+                            Tl.play();
+                        } else {
+                            ScrollTrigger.create({
+                                trigger: t,
+                                start: "top 80%",
+                                end: "bottom bottom",
+                                animation: Tl,
+                                toggleActions: "play none play none",
+                                // scrub: 1.3,
+                                // markers: true,
+                            });
+                        }
+                    }
+                    if (t.dataset.effect == "header") {
+                        const e = t.querySelectorAll(".char");
+                        e.forEach((m) =>
+                            gsap.set(m.parentNode, {
+                                perspective: 1e3,
+                            })
+                        );
+                        gsap.set(t, { scaleX: 0 });
+                        Tl = gsap.timeline({
+                            defaults: {
+                                ease: "Power3.easeOut",
+                                duration: 0.7,
+                            },
+                        });
+                        Tl.set(t, { opacity: 1 })
+                            .fromTo(t, { scaleX: 0 }, { scaleX: 1 })
+                            .fromTo(
+                                e,
+                                {
+                                    "will-change": "opacity,",
+                                    opacity: 0,
+                                },
+                                {
+                                    opacity: 1,
+                                    stagger: 0.06,
+                                }
+                            )
+                            .pause();
+                        if (pos.top < window.innerHeight) {
+                            Tl.play();
+                        } else {
+                            ScrollTrigger.create({
+                                trigger: t,
+                                start: "top 80%",
+                                end: "bottom bottom",
+                                animation: Tl,
+                                toggleActions: "play none play none",
+                                // scrub: 1.3,
+                                // markers: true,
+                            });
+                        }
+                    }
+                    if (t.dataset.effect == "paragraph") {
+                        Tl.set(t, { opacity: 1 });
+                        Tl.fromTo(
+                            t.querySelectorAll(".word"),
+                            {
+                                "will-change": "opacity",
+                                opacity: 0.1,
+                            },
+                            {
+                                ease: "none",
+                                opacity: 1,
+                                stagger: 0.05,
+                            }
+                        ).pause();
+                        if (pos.top < window.innerHeight) {
+                            Tl.timeScale(2).play();
+                        } else {
+                            ScrollTrigger.create({
+                                trigger: t,
+                                start: "top 90%",
+                                end: "bottom 90%",
+                                scrub: 1.3,
+                                animation: Tl,
+                                // markers: true,
+                            });
+                        }
+                    }
+                    if (t.dataset.effect == "squeeze") {
+                        Tl.set(t, { opacity: 1 });
+                        const e = t.querySelectorAll(".char");
+                        Tl.fromTo(
+                            t,
+                            {
+                                letterSpacing: "-0.1em",
+                                opacity: 0,
+                            },
+                            {
+                                letterSpacing: "-0.05em",
+                                opacity: 1,
+                            }
+                        ).pause();
+                        if (pos.top < window.innerHeight) {
+                            setTimeout(() => {
+                                Tl.timeScale(0.5).play();
+                            }, 800);
+                        } else {
+                            ScrollTrigger.create({
+                                trigger: t,
+                                start: "top 80%",
+                                end: "bottom 50%",
+                                scrub: 1.3,
+                                animation: Tl,
+                                // markers: true,
+                            });
+                        }
+                    }
                 }
-
                 //set animation on section enter
                 ScrollTrigger.create({
                     trigger: t,
@@ -34,7 +177,6 @@ Webflow.push(function () {
                 });
             });
         }
-
         //position anim -from
         if ($("[data-position]").length) {
             let el_position = document.querySelectorAll("[data-position]");
@@ -50,7 +192,6 @@ Webflow.push(function () {
                     },
                 });
                 Tl.set(t, { opacity: 1 });
-
                 if (pos.top > window.innerHeight) {
                     ScrollTrigger.create({
                         trigger: t,
@@ -63,7 +204,6 @@ Webflow.push(function () {
                         // markers: true,
                     });
                 }
-
                 // console.log(t.dataset.position);
                 function animload() {
                     if (t.dataset.position == "left") {
@@ -141,14 +281,12 @@ Webflow.push(function () {
                             }
                         ).pause();
                     }
-
                     if (pos.top < window.innerHeight) {
                         setTimeout(() => {
                             Tl.timeScale(0.5).play();
                         }, 1200);
                     }
                 }
-
                 //set animation on section enter
                 ScrollTrigger.create({
                     invalidateOnRefresh: true,
@@ -161,11 +299,9 @@ Webflow.push(function () {
                 });
             });
         }
-
         //parallax
         if ($("[data-parallax]").length) {
             const parallax_box = document.querySelectorAll("[data-parallax]");
-
             parallax_box.forEach(function (prel) {
                 const data = prel.dataset.parallax;
                 const parallax_el = prel.querySelectorAll("[data-speed]");
@@ -233,7 +369,6 @@ Webflow.push(function () {
                 }
             });
         }
-
         if ($("[data-reveal]").length) {
             const reveal_box = document.querySelectorAll("[data-reveal]");
             reveal_box.forEach(function (revEl) {
@@ -248,7 +383,6 @@ Webflow.push(function () {
                 });
             });
         }
-
         if ($("[data-scroll]").length) {
           const scroll_box = document.querySelectorAll("[data-scroll]");
           scroll_box.forEach(function (revEl) {
@@ -266,6 +400,124 @@ Webflow.push(function () {
           });
         }
 
+        if ($(".the-caio-sec").length) {
+          let caioTl = gsap.timeline();
+          const el = document.querySelector(".the-caio-sec");
+          let heading = el.querySelector(".the-caio-big-txt"),
+            img = el.querySelector(".the-caio-img-wpr"),
+            para = el.querySelector(".the-caio-txt-wpr>p"),
+            btn = el.querySelector(".btn-wpr"),
+            sunheadline = el.querySelector(".the-caio-subtxt>p"),
+            items = el.querySelector(".the-caio_grid_box");
+
+          gsap.set(heading, { opacity: 0, letterSpacing: "-0.1em" });
+          Splitting({ target: para, by: "chars" });
+          gsap.set(para, { opacity: 0 });
+          gsap.set(para.querySelectorAll(".word"), {
+            "will-change": "opacity",
+            opacity: 0.1,
+          });
+          gsap.set(img, { opacity: 0, yPercent: 25, scale: 0.7, rotate: -25 });
+          gsap.set(btn, { opacity: 0, yPercent: 100 });
+          gsap.set(sunheadline, { opacity: 0 });
+          gsap.set(items, { opacity: 0 });
+          gsap.set(items.querySelectorAll(".the-caio_item"), {
+            opacity: 0,
+            yPercent: 100,
+          });
+          let itemTl = gsap.timeline();
+          itemTl
+            .fromTo(
+              items.querySelectorAll(".the-caio_item"),
+              { opacity: 0, yPercent: 100 },
+              {
+                opacity: 1,
+                yPercent: 0,
+                stagger: 0.1,
+              }
+            )
+            .pause();
+
+          caioTl
+            .to(heading, {
+              duration: 0.5,
+              letterSpacing: "-0.05em",
+              opacity: 1,
+            })
+            .to(
+              img,
+              {
+                duration: 1,
+                opacity: 1,
+                yPercent: 0,
+              },
+              "<"
+            )
+            .set(para, { opacity: 1 }, "<")
+            .to(
+              para.querySelectorAll(".word"),
+              {
+                ease: "none",
+                duration: 0.3,
+                opacity: 1,
+                stagger: 0.01,
+              },
+              "<"
+            )
+            .to(btn, { delay: 0.5, duration: 0.3, opacity: 1, yPercent: 0 }, "<")
+            .to(heading, {
+              delay: 0.1,
+              duration: 0.5,
+              opacity: 0,
+              letterSpacing: "-0.1em",
+            })
+            .to([para, btn], { duration: 0.5, opacity: 0, yPercent: 25 }, "<")
+            .to(
+              img,
+              { duration: 1, yPercent: 0, xPercent: 50, scale: 1, rotate: 0 },
+              "-=0.3"
+            )
+            .to(sunheadline, { duration: 0.5, opacity: 1 }, "-=0.3")
+            .to(sunheadline, { delay: 0.15, duration: 0.3, opacity: 0 })
+            .to(
+              img,
+              {
+                duration: 1,
+                yPercent: 0,
+                xPercent: -25,
+                scale: 0.8,
+                rotate: -45,
+                onEnd: () => itemTl.reverse(),
+              },
+              "<"
+            )
+            .to(items, {
+              opacity: 1,
+              onStart: () => itemTl.restart(),
+            },"-=0.5");
+
+          caioTl.pause();
+
+          ScrollTrigger.create({
+            trigger: ".sec-cntnt",
+            start: "top 50%",
+            end: "+=300%",
+            animation: caioTl,
+            toggleActions: "play none play none",
+            invalidateOnRefresh: true,
+            scrub: 1.3,
+            // markers: true,
+          });
+
+          ScrollTrigger.create({
+            trigger: el,
+            start: "top -20%",
+            end: "+=250%",
+            invalidateOnRefresh: true,
+            pin: true,
+            // markers: true,
+          });
+        }
     }
 
     //page loader
@@ -274,7 +526,6 @@ Webflow.push(function () {
         gsap.set(".ld2", { x: -70, opacity: 0 });
         gsap.set(".ld3", { y: -50, opacity: 0 });
         gsap.set(".ld4", { y: 50, opacity: 0 });
-
         var loader_tl = gsap.timeline({ repeat: -1 });
         loader_tl
             .to(".ld1", {
@@ -295,7 +546,6 @@ Webflow.push(function () {
                 scale: 1,
                 opacity: 1,
             });
-
         function loader() {
             loader_tl
                 .repeat(0)
@@ -326,7 +576,6 @@ Webflow.push(function () {
                             gsap.set("body", { overflow: "auto" });
                             gsap.to(".banner .container", { delay: 0.3, opacity: 1 });
                         }
-
                         page_anim();
                     },
                 })
@@ -340,11 +589,9 @@ Webflow.push(function () {
                     "<"
                 );
         }
-
         //for home page
         if ($(".reveal_outer").length) {
             document.body.style.overflow = "hidden";
-
             //mouse scroll action
             var scrollPos = "down";
             window.addEventListener("wheel", (event) => {
@@ -367,7 +614,6 @@ Webflow.push(function () {
                 startTime: 0,
                 dt: 0,
             };
-
             Reveal.initialize({
                 mouseWheel: false,
                 touch: false,
@@ -390,14 +636,11 @@ Webflow.push(function () {
                 maxScale: 1,
             });
             Reveal.layout();
-
             Reveal.on("ready", (event) => {
                 //progressbar
                 loader();
-
                 let parent = document.querySelector(".reveal_outer"),
                     els = parent.querySelectorAll(".slides>.rv-section");
-
                 let list = document.createElement("ul");
                 list.classList.add("list");
                 parent.append(list);
@@ -407,7 +650,6 @@ Webflow.push(function () {
                     list.append(li);
                 });
                 list.querySelectorAll("li")[event.indexh].classList.add("active");
-
                 //split by lines
                 document.querySelectorAll(".cover_fig_outer .ban-h1").forEach((tr) => {
                     Splitting({ target: tr, by: "lines" });
@@ -417,7 +659,6 @@ Webflow.push(function () {
                         wrd.querySelector(".wrd_anim").style.cssText = wrd.style.cssText;
                     });
                 });
-
                 setTimeout(() => {
                     //enable reveal
                     touch.listening = true;
@@ -426,10 +667,8 @@ Webflow.push(function () {
                     });
                 }, 3000);
             });
-
             Reveal.on("slidechanged", (event) => {
                 ScrollTrigger.refresh();
-
                 Reveal.configure({
                     mouseWheel: false,
                 });
@@ -440,7 +679,6 @@ Webflow.push(function () {
                 });
                 list.querySelectorAll("li")[event.indexh].classList.add("active");
             });
-
             Reveal.on("slidetransitionend", (event) => {
                 Reveal.configure({
                     mouseWheel: true,
@@ -459,7 +697,6 @@ Webflow.push(function () {
                     }
                 }, 1500);
             });
-
             window.addEventListener("resize", () => {
                 Reveal.configure({
                     width: window.innerWidth,
@@ -467,7 +704,6 @@ Webflow.push(function () {
                 });
                 ScrollTrigger.refresh();
             });
-
             ScrollTrigger.create({
                 trigger: ".reveal_outer",
                 start: "top top",
@@ -488,7 +724,6 @@ Webflow.push(function () {
                 },
                 // markers: true,
             });
-
             document.addEventListener("touchstart", handleTouchStart, false);
             document.addEventListener("touchmove", handleTouchMove, false);
             document.addEventListener("touchend", handleTouchEnd, false);
@@ -524,7 +759,6 @@ Webflow.push(function () {
                 const t = e.changedTouches[0];
                 touch.dx = t.pageX - touch.startX;
                 touch.dy = t.pageY - touch.startY;
-
                 if (Math.abs(touch.dy) < touch.threshold) {
                     touch.listening = false;
                     setTimeout(() => {
