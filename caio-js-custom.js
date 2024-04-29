130c38e7bc5b998bcbbd99d6a747e3f0169a50a7
arijit-webskitters <155645185+arijit-webskitters@users.noreply.github.com>
2024-04-29 17:19:40 +0530

    Update caio-js-custom.js

--- caio-js-custom.js
+++ caio-js-custom.js
@@ -10,7 +10,7 @@ var wheelOpt = supportsPassive ? { passive: false } : false;
 var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
 
 function disableScroll() {
-  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  // window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
   window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
   window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
   // window.addEventListener('keydown', preventDefaultForScrollKeys, false);
@@ -18,7 +18,7 @@ function disableScroll() {
 
 // call this to Enable
 function enableScroll() {
-  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  // window.removeEventListener('DOMMouseScroll', preventDefault, false);
   window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
   window.removeEventListener('touchmove', preventDefault, wheelOpt);
   // window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
