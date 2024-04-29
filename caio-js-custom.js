a83be56b821198969c53d933c15bf5749d700f6a
arijit-webskitters <155645185+arijit-webskitters@users.noreply.github.com>
2024-04-29 17:18:11 +0530

    Update caio-js-custom.js

--- caio-js-custom.js
+++ caio-js-custom.js
@@ -1,8 +1,19 @@
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
 function disableScroll() {
   window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
   window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
   window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+  // window.addEventListener('keydown', preventDefaultForScrollKeys, false);
 }
 
 // call this to Enable
@@ -10,7 +21,7 @@ function enableScroll() {
   window.removeEventListener('DOMMouseScroll', preventDefault, false);
   window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
   window.removeEventListener('touchmove', preventDefault, wheelOpt);
-  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+  // window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
 }
 
 var Webflow = Webflow || [];
